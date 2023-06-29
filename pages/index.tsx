5bdf02a0bbf3572c08b7beb06db6ea14baff8608
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-28 20:32:51 -0500

    homePage temporal y sidebar lateral

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+import { Typography, Grid, Card, CardActionArea, CardMedia } from "@mui/material";
 import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { initialData } from "@/database/products";
 
 
 export default function Home() {
@@ -7,6 +8,24 @@ export default function Home() {
     <ShopLayout title={"Teslo-App - Home"} pageDescription={"Encuentra los mejores productos en Teslo-App"}>
       <Typography variant="h1" component={'h1'}>Tienda</Typography>
       <Typography variant="h2" sx={{ mb:1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+          {
+            initialData.products.map( product => (
+              <Grid item xs={6} sm={4} key={ product.slug }>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia 
+                      component='img'
+                      image={`products/${ product.images[0] }`}
+                      alt={ product.title }
+                    />
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))
+          }
+      </Grid>
     </ShopLayout>
   )
 }

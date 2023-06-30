aa8e0c6afd14296ae1fd14097169df046c365f99
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-29 21:04:12 -0500

    Iniciando la pagina de carrito de compras

---
+++ components/cart/CardList.tsx
@@ -0,0 +1,23 @@
+import { initialData } from "@/database/products"
+import { Typography } from "@mui/material"
+
+
+const productsInCart = [
+    initialData.products[0],
+    initialData.products[1],
+    initialData.products[2],
+
+]
+
+
+export const CardList = () => {
+  return (
+    <>
+        {
+            productsInCart.map( product => (
+                <Typography key={ product.slug }>{ product.title }</Typography>
+            ))
+        }
+    </>
+  )
+}
